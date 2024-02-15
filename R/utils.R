08124c7acdc56f89f0b094dfc23e5e564a54cf75
wutao <1650464505@qq.com>
2024-02-15 11:13:45 +0800

    add ac mail

--- R/utils.R
+++ R/utils.R
@@ -63,7 +63,8 @@ check_user_pass <- function(x){
 check_user_mail <- function(x) {
   ###检测是否是 edu 或者 edu.cn 结尾的
   check1 <- stringi::stri_detect(regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z-a-z]{2,}$", str = as.character(x))
-  check2 <- stringi::stri_detect(regex = "edu\\..+$|\\.edu$", str=as.character(x))
+  check2 <- stringi::stri_detect(regex = "edu\\..+$|\\.edu$|ac\\..+$|\\.ac$", 
+                                 str=as.character(x))
   check1 & check2
 }
 
